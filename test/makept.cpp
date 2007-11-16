24796efca05f77f6a2d56851ef04fa261069e3e2
janehu <janehu@6162379e-bd28-0410-9a7d-b7f4dcfcad3c>
2007-11-16 19:40:16 +0000

    make new directory for tests, move webcut test there, and add new OCC test
    git-svn-id: https://svn.mcs.anl.gov/repos/ITAPS/cgm/trunk@1405 6162379e-bd28-0410-9a7d-b7f4dcfcad3c

---
+++ test/makept.cpp
@@ -0,0 +1,138 @@
+/**
+ * \file makept.cpp
+ *
+ * \brief makept, another simple C++ driver for CGM
+ *
+ * This program acts as a simple driver for CGM.  It reads in a geometry,
+ * performs imprints between all the bodies, merges them, and writes information
+ * on the results.  It also performs pairwise intersections between the
+ * bodies to check for overlaps.  Results are written to stardard output.
+ *
+ */
+#include "config.h"
+#include "CpuTimer.hpp"
+#include "GeometryModifyTool.hpp"
+#include "GeometryQueryTool.hpp"
+#include "OCCQueryEngine.hpp"
+#include "CubitUtil.hpp"
+#include "CubitMessage.hpp"
+#include "CubitDefines.h"
+#include "RefEntity.hpp"
+#include "Body.hpp"
+#include "RefVolume.hpp"
+#include "RefFace.hpp"
+#include "RefEdge.hpp"
+#include "RefVertex.hpp"
+#include "CubitObserver.hpp"
+#include "CastTo.hpp"
+#include "OCCModifyEngine.hpp"
+#include "AppUtil.hpp"
+#include "RefEntityFactory.hpp"
+#include "RefEdge.hpp"
+
+// forward declare some functions used and defined later
+CubitStatus read_geometry(int, char **);
+CubitStatus make_Point();
+// macro for printing a separator line
+#define PRINT_SEPARATOR   PRINT_INFO("=======================================\n");
+
+
+// main program - initialize, then send to proper function
+int main (int argc, char **argv)
+{
+
+  CubitObserver::init_static_observers();
+    // Initialize the GeometryTool
+  
+  CGMApp::instance()->startup( argc, argv );
+  OCCQueryEngine::instance();
+  OCCModifyEngine::instance();
+
+    // If there aren't any file arguments, print usage and exit
+  //if (argc == 1) {
+  //  PRINT_INFO("Usage: mergechk <geom_file> [<geom_file> ...]\n");
+  //  exit(0);
+  //}
+  
+  CubitStatus status = CUBIT_SUCCESS;
+
+
+
+  //Do make point.
+  status = make_Point();
+  if (status == CUBIT_FAILURE) 
+     PRINT_INFO("Operation Failed");
+
+  int ret_val = ( CubitMessage::instance()->error_count() );
+  if ( ret_val > 0 )
+  {
+    PRINT_ERROR("Errors found during Mergechk session.\n");
+  }
+  return ret_val;
+  
+}
+
+/// attribs module: list, modify attributes in a give model or models
+/// 
+/// Arguments: file name(s) of geometry files in which to look
+///
+CubitStatus read_geometry(int num_files, char **argv) 
+{
+  CubitStatus status = CUBIT_SUCCESS;
+  GeometryQueryTool *gti = GeometryQueryTool::instance();
+  assert(gti);
+  int i;
+  
+  PRINT_SEPARATOR;
+
+  for (i = 0; i < num_files; i++) {
+    status = gti->import_solid_model(argv[i], "OCC");
+    if (status != CUBIT_SUCCESS) {
+      PRINT_ERROR("Problems reading geometry file %s.\n", argv[i]);
+    }
+  }
+  PRINT_SEPARATOR;
+
+  return CUBIT_SUCCESS;
+}
+
+CubitStatus make_Point()
+{
+  GeometryQueryTool *gti = GeometryQueryTool::instance();
+  GeometryModifyTool *gmti = GeometryModifyTool::instance();
+
+  OCCQueryEngine::instance();
+  OCCModifyEngine::instance();
+
+  // Read in the geometry from files specified on the command line
+  char *argv = "./point.occ";
+  CubitStatus status = read_geometry(1, &argv);
+  if (status == CUBIT_FAILURE) exit(1);
+
+  // Read in the geometry from files specified on the command line
+  CubitVector vector(10,10,10); 
+  gmti->make_RefVertex(vector,5);
+
+  CubitStatus rsl = CUBIT_SUCCESS;
+  DLIList<RefEntity*> ref_entity_list;
+  int num_ents_exported=0;
+  const CubitString cubit_version="10.2";
+  const char * filename = "point.occ";
+  const char * filetype = "OCC";
+  rsl = gti->export_solid_model(ref_entity_list, filename, filetype, 
+                                 num_ents_exported, cubit_version);
+
+  //delete all entities
+  DLIList<Body*> bodies;
+  gti->bodies(bodies);
+  DLIList<RefEntity*> free_entities;
+  gti->get_free_ref_entities(free_entities);
+  gti->delete_Body(bodies);
+
+  for (int j = free_entities.size(); j--;)
+    {
+      gti->delete_RefEntity( free_entities.get_and_step());
+    }
+
+  return rsl;
+}
