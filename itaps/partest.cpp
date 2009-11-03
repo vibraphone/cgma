d0b67b8d06dc23013c94d27effdcd5d78d41d50f
hongjun <hongjun@6162379e-bd28-0410-9a7d-b7f4dcfcad3c>
2009-11-03 19:22:32 +0000

    o partest can deal with default input cases o "Moto.brep" geometry file is added for default test o removed partest from TESTS and added to check_PROGRAMS o partest prints out memory usage
    git-svn-id: https://svn.mcs.anl.gov/repos/ITAPS/cgm/trunk@3274 6162379e-bd28-0410-9a7d-b7f4dcfcad3c

--- itaps/partest.cpp
+++ itaps/partest.cpp
@@ -1,8 +1,15 @@
 // Test parallel iGeom
+// July 09
+// Author : Hongjun Kim
+// read OCC geometry file and distribute it to remote processors
+// with various methods
 
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
+#include <sys/resource.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
 
 #include "CGMmpi.h"
@@ -10,6 +17,8 @@
 
 #define IGEOM_ASSERT(ierr) if (ierr!=0) printf("igeom assert\n");
 #define IGEOM_NULL 0
+
+void util_getrusage(struct rusage &r_usage);
 
 int main(int argc, char* argv[]){
   int rank, size;
@@ -34,13 +43,13 @@ int main(int argc, char* argv[]){
     nMethod = atoi(argv[2]);
   }
   else {
-    if (rank == 0) printf("Usage: %s [<filename>] [<send_methond>]\n", argv[0]);
-    if (argc != 1 || argc != 2) return 1;
     if (rank == 0) {
-      printf("  No file specified.  Defaulting to: %s\n", "Moto.brep");
-      printf("  No send method specified.  Defaulting to: read_delete\n");
+      printf("Usage: %s [<filename>] [<send_methond>]\n", argv[0]);
+      printf("No file specified.  Defaulting to: %s\n", "Moto.brep");
+      printf("No send method specified.  Defaulting to: read_delete\n");
     }
-    filename = "../../../test_files/Moto.brep";
+    filename = "../test/Moto.brep";
+    nMethod = 1;
   }
   
   // set geometry send method
@@ -59,14 +68,78 @@ int main(int argc, char* argv[]){
   // do body partitioning with round robin distribution
   options += "PARTITION=GEOM_DIMENSION;PARTITION_VAL=3;PARTITION_DISTRIBUTE;";
 
+  // 
+  struct rusage r_usage;
+  long int rss_before, rss_after, size_before, size_after;
+  static size_t pagesize = getpagesize();
+  //if (rank == 0) {
+    util_getrusage(r_usage);
+    rss_before = r_usage.ru_maxrss*pagesize;
+    size_before = r_usage.ru_idrss*pagesize;
+    //}
+
   double tStart = MPI_Wtime();
   iGeom_load(igeom, filename, options.c_str(), &ierr, strlen(filename), options.length());
 	    
   MPI_Barrier(MPI_COMM_WORLD); 
   double tEnd = MPI_Wtime();
 
+  //if (rank == 0) {
+    util_getrusage(r_usage);
+    rss_after = r_usage.ru_maxrss*pagesize;
+    size_after = r_usage.ru_idrss*pagesize;
+
+    printf("proc=%d, rss_before=%ld, size_before=%ld\n", rank, rss_before, size_before);
+    printf("proc=%d, rss_after=%ld, size_after=%ld\n", rank, rss_after, size_after);
+    //}
+
   IGEOM_ASSERT(ierr);
   
   printf("Done. Geometry load time is %f\n", tEnd - tStart);
   MPI_Finalize();
+}
+
+void util_getrusage(struct rusage &r_usage)
+{
+  getrusage(RUSAGE_SELF, &r_usage);
+  
+  // this machine doesn't return rss - try going to /proc
+  // print the file name to open
+  if (r_usage.ru_maxrss == 0) {
+    char file_str[4096], dum_str[4096];
+    int file_ptr = -1, file_len;
+    file_ptr = open("/proc/self/stat", O_RDONLY);
+    file_len = read(file_ptr, file_str, sizeof(file_str)-1);
+    if (file_len == 0) return;
+    close(file_ptr);
+    file_str[file_len] = '\0';
+    
+    // read the preceeding fields and the ones we really want...
+    int dum_int;
+    unsigned int dum_uint, vm_size, rss;
+    static int page_size = getpagesize();
+    int num_fields = sscanf(file_str, 
+			    "%d " // pid
+			    "%s " // comm
+			    "%c " // state
+			    "%d %d %d %d %d " // ppid, pgrp, session, tty, tpgid
+			    "%u %u %u %u %u " // flags, minflt, cminflt, majflt, cmajflt
+			    "%d %d %d %d %d %d " // utime, stime, cutime, cstime, counter, priority
+			    "%u %u " // timeout, itrealvalue
+			    "%d " // starttime
+			    "%u %u", // vsize, rss
+			    &dum_int, 
+			    dum_str, 
+			    dum_str, 
+			    &dum_int, &dum_int, &dum_int, &dum_int, &dum_int, 
+			    &dum_uint, &dum_uint, &dum_uint, &dum_uint, &dum_uint,
+			    &dum_int, &dum_int, &dum_int, &dum_int, &dum_int, &dum_int, 
+			    &dum_uint, &dum_uint, 
+			    &dum_int,
+			    &vm_size, &rss);
+    if (num_fields == 24) {
+      r_usage.ru_maxrss = rss/page_size;
+      r_usage.ru_idrss = vm_size/page_size;
+    }
+  }
 }
