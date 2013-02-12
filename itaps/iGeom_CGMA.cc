74ecde47303d9a694e018d9e4f14f0677c0c6ea8
janehu <janehu@6162379e-bd28-0410-9a7d-b7f4dcfcad3c>
2013-02-12 15:54:42 +0000

    Realized that this version is only cubit13.1 on cgm, the release flag itself will be enough for meshkit, and code doesn't need use cubit 12.2 and under.
    git-svn-id: https://svn.mcs.anl.gov/repos/ITAPS/cgm/trunk@5996 6162379e-bd28-0410-9a7d-b7f4dcfcad3c

--- itaps/iGeom_CGMA.cc
+++ itaps/iGeom_CGMA.cc
@@ -6094,30 +6094,22 @@ iGeom_rotateEnt( iGeom_Instance instance,
 ITAPS_API void
 iGeom_reflectEnt( iGeom_Instance instance,
                   /*inout*/ iBase_EntityHandle geom_entity,
-#if CUBIT_MAJOR_API == 13
                   /*in*/ double point_x,
                   /*in*/ double point_y,
                   /*in*/ double point_z,
-#endif
                   /*in*/ double plane_normal_x,
                   /*in*/ double plane_normal_y,
                   /*in*/ double plane_normal_z,
                   int* err )
 {
   CubitVector this_plane(plane_normal_x, plane_normal_y, plane_normal_z);
-#if CUBIT_MAJOR_API == 13
   CubitVector point(point_x, point_y, point_z);
-#endif
   Body *this_bod = dynamic_cast<Body*>(ENTITY_HANDLE(geom_entity));
   DLIList<Body*> bods;
   bods.append(this_bod);
   CubitStatus result;
   if (NULL != this_bod) {
-#if CUBIT_MAJOR_API == 13
     result = gqt->reflect(bods, point , this_plane);
-#else
-    result = gqt->reflect(bods, this_plane);
-#endif
     if (CUBIT_SUCCESS != result) {
       ERROR(iBase_FAILURE, "Failed to reflect body.");
     }
@@ -6127,11 +6119,7 @@ iGeom_reflectEnt( iGeom_Instance instance,
   
   BasicTopologyEntity *this_bte = dynamic_cast<BasicTopologyEntity*>(ENTITY_HANDLE(geom_entity));
   if (NULL != this_bte) {
-#if CUBIT_MAJOR_API == 13
     result = gqt->reflect(this_bte, point, this_plane);
-#else
-    result = gqt->reflect(this_bte, this_plane);
-#endif
     if (CUBIT_SUCCESS != result) {
       ERROR(iBase_FAILURE, "Failed to reflect entity.");
     }
@@ -6145,28 +6133,20 @@ iGeom_reflectEnt( iGeom_Instance instance,
 ITAPS_API void
 iGeom_scaleEnt( iGeom_Instance instance,
                 /*inout*/ iBase_EntityHandle geom_entity,
-#if CUBIT_MAJOR_API == 13
                 /*in*/ double point_x,
                 /*in*/ double point_y,
                 /*in*/ double point_z,
-#endif
                 /*in*/ double scale_x,
                 /*in*/ double scale_y,
                 /*in*/ double scale_z,
                 int* err )
 {
   CubitVector factor(scale_x, scale_y, scale_z);
-#if CUBIT_MAJOR_API == 13
   CubitVector point(point_x, point_y, point_z);
-#endif
   Body *this_bod = dynamic_cast<Body*>(ENTITY_HANDLE(geom_entity));
   CubitStatus result;
   if (NULL != this_bod) {
-#if CUBIT_MAJOR_API == 13
     result = gqt->scale(this_bod, point, factor);
-#else
-    result = gqt->scale(this_bod, factor);
-#endif
     if (CUBIT_SUCCESS != result) {
       ERROR(iBase_FAILURE, "Failed to scale body.");
     }
@@ -6180,11 +6160,7 @@ iGeom_scaleEnt( iGeom_Instance instance,
     // there is no body, it's a free entity and we can move it anyway
   Body *this_body = this_bte->body();
   if (NULL == this_body && NULL != this_bte) {
-#if CUBIT_MAJOR_API == 13
     result = gqt->scale(this_bte, point, factor);
-#else
-    result = gqt->scale(this_bte, factor);
-#endif
     if (CUBIT_SUCCESS != result) {
       ERROR(iBase_FAILURE, "Failed to scale entity.");
     }
@@ -6200,11 +6176,7 @@ iGeom_scaleEnt( iGeom_Instance instance,
     }
     if (num_sibs == 1) {
         // ok to scale the body instead
-#if CUBIT_MAJOR_API == 13
       result = gqt->scale(this_body, point, factor);
-#else
-      result = gqt->scale(this_body, factor);
-#endif
       if (CUBIT_SUCCESS != result) {
         ERROR(iBase_FAILURE, "Failed to scale body even only one entity of that"
                            " dimension in the body.");
